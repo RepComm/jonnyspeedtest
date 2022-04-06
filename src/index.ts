94ac8c309d51f6e2042446016e9ec132505e4a8d
Jon <rccommandotac@gmail.com>
2022-04-05 21:22:22 -0500

    bare bones functioning

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,42 @@
 
-export default class MyPlugin {
-  constructor () {
-    console.log("Hello World");
-  }
+import { createServer } from "http";
+
+function main() {
+
+  let data = Buffer.alloc(1024, " ");
+
+  let kbSize = 1024 * 10;
+
+  let timeStart;// = Date.now();
+  let server = createServer((req, res) => {
+    if (req.url.endsWith("test")) {
+      timeStart = Date.now();
+
+
+      res.write("start");
+
+      for (let i = 0; i < kbSize; i++) {
+        res.write(data);
+      }
+
+      res.write("end");
+
+    }
+    res.end(()=>{
+      let timeEnd = Date.now();
+      let timeEnlapsed = timeEnd - timeStart;
+      let timeEnlapsedString = (timeEnlapsed / 1000).toFixed(1);
+  
+      let mbps = (kbSize / (timeEnlapsed / 1000 * 1000)).toFixed(0);
+      
+      console.log(`Took ${timeEnlapsedString}s to send ${kbSize}kb, averaging ${mbps}mbps`);
+
+    });
+
+  });
+
+  server.listen(8080);
+
 }
 
-let myPluginInst = new MyPlugin();
+main();
