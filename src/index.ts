8633ada92ee0c0fbc9c1757893dd515a8a055c53
Jon <rccommandotac@gmail.com>
2022-04-06 13:14:38 -0500

    fetch vs html end points

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,61 @@
 
 import { createServer } from "http";
+import { parse } from "url";
 
 function main() {
 
   let data = Buffer.alloc(1024, " ");
 
-  let kbSize = 1024 * 10;
+  let kbSize = 1024;
 
-  let timeStart;// = Date.now();
   let server = createServer((req, res) => {
-    if (req.url.endsWith("test")) {
-      timeStart = Date.now();
-
-
-      res.write("start");
-
+    let timeStart = Date.now();
+    let urlData = parse(req.url, true);
+    
+    if (urlData.query["payloadSizeKB"]) {
+      kbSize = parseInt(urlData.query["payloadSizeKB"] as string);
+      
+      res.write("start", ()=>{
+      });
+  
       for (let i = 0; i < kbSize; i++) {
         res.write(data);
       }
-
-      res.write("end");
-
-    }
-    res.end(()=>{
-      let timeEnd = Date.now();
-      let timeEnlapsed = timeEnd - timeStart;
-      let timeEnlapsedString = (timeEnlapsed / 1000).toFixed(1);
   
-      let mbps = (kbSize / (timeEnlapsed / 1000 * 1000)).toFixed(0);
-      
-      console.log(`Took ${timeEnlapsedString}s to send ${kbSize}kb, averaging ${mbps}mbps`);
+      res.write("end\n", ()=>{
+        let timeEnd = Date.now();
+        let timeEnlapsed = timeEnd - timeStart;
+        let timeEnlapsedString = (timeEnlapsed / 1000).toFixed(1);
+    
+        let kbps = (kbSize / (timeEnlapsed / 1000));
+        let mbps = kbps/1000;
+        
+        let msg = `Took ${timeEnlapsedString}s to send ${kbSize}kb, averaging ${kbps.toFixed(0)}kbps or ${mbps.toFixed(0)}mbps`;
+        console.log(msg);
+        res.write(msg);
+        res.end();
+      });
 
-    });
+    } else {
+      res.end(`
+      <html>
+      <head></head>
+      <body>
+      <script>
+      let timeStart = Date.now();
+
+      fetch(window.location.host + "/?payloadSizeKB=16000").then(()=>{
+        let timeEnd = Date.now();
+        let timeEnlapsed = timeEnd - timeStart;
+        let sEnlapsed = timeEnlapsed / 1000;
+        console.log(sEnlapsed.toFixed(2) + "s")
+      });
+      </script>
+      </body>
+      </html>
+      `);
+    }
+
 
   });
 
